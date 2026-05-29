fea1e0ad0b204ac9d892628fe521ca732311d93f
Joshua Soon <joshuaslj231094@gmail.com>
2026-05-29 19:50:23 +0800

    openCV with GTK3 in flake.

--- flake.nix
+++ flake.nix
@@ -30,42 +30,45 @@
           });
     in {
       # Development environment output
-      devShells = forAllSystems ({ pkgs, system, enableCuda, ... }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = with pkgs;
-            [
-              gcc14 # The GNU Compiler Collection
-              cmake
-              cxxopts
-              opencv
-              onnxruntime
-              glaze
-              openssl
-            ] ++ lib.optionals enableCuda [ cudaPackages.cudatoolkit ];
+      devShells = forAllSystems ({ pkgs, system, enableCuda, ... }:
+        let opencvWithGui = pkgs.opencv.override { enableGtk3 = true; };
+        in {
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = with pkgs;
+              [
+                gcc14 # The GNU Compiler Collection
+                cmake
+                cxxopts
+                opencvWithGui
+                onnxruntime
+                glaze
+                openssl
+              ] ++ lib.optionals enableCuda [ cudaPackages.cudatoolkit ];
 
-          shellHook = ''
-            bash ./nix_env_setup.sh
-            echo "You are in a GCC-based nix shell"
-            export CC=${pkgs.gcc14}/bin/gcc
-            export CXX=${pkgs.gcc14}/bin/g++
-          '' + pkgs.lib.optionalString enableCuda ''
-            export PATH=${pkgs.cudaPackages.cudatoolkit}/bin:$PATH
-            export CUDA_HOME=${pkgs.cudaPackages.cudatoolkit}
-            export CUDA_LIB=${pkgs.cudaPackages.cudatoolkit}/lib
-          '' + ''
-            export DOG_TRACKER_CMAKE_FLAGS="-DUSE_CUDA=${
-              if enableCuda then "ON" else "OFF"
-            }"
-          '';
-        };
-      });
+            shellHook = ''
+              bash ./nix_env_setup.sh
+              echo "You are in a GCC-based nix shell"
+              export CC=${pkgs.gcc14}/bin/gcc
+              export CXX=${pkgs.gcc14}/bin/g++
+            '' + pkgs.lib.optionalString enableCuda ''
+              export PATH=${pkgs.cudaPackages.cudatoolkit}/bin:$PATH
+              export CUDA_HOME=${pkgs.cudaPackages.cudatoolkit}
+              export CUDA_LIB=${pkgs.cudaPackages.cudatoolkit}/lib
+            '' + ''
+              export DOG_TRACKER_CMAKE_FLAGS="-DUSE_CUDA=${
+                if enableCuda then "ON" else "OFF"
+              }"
+            '';
+          };
+        });
 
       packages = forAllSystems ({ pkgs, enableCuda, ... }: {
         default = let
+          opencvWithGui = pkgs.opencv.override { enableGtk3 = true; };
           buildDependencies = with pkgs; [ gcc14 cmake ];
           cppDependencies = with pkgs;
-            [ cxxopts opencv onnxruntime glaze openssl ]
+            [ cxxopts opencvWithGui onnxruntime glaze openssl ]
             ++ lib.optionals enableCuda [ cudaPackages.cudatoolkit ];
           projectName = "turbo_pi_dog_tracker";
         in pkgs.stdenv.mkDerivation {
